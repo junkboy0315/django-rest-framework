297871ccc089f854f5aa3952184cb5fe0a9335f6
Shota Tamura <tamura@yuuniworks.com>
2019-02-08 15:57:39 +0900

    views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 
-# Create your views here.
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    - 全ての snippets を表示する。
+    - 新しい snippet を作成する
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=400)
+        serializer.save()
+        return JsonResponse(serializer.data, status=201)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    単一のスニペットの、取得・更新・削除を行う。
+    """
+    try:
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if not serializer.is_valid():
+            return JsonResponse(serializer.errors, status=400)
+        serializer.save()
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'DELETE':
+        snippet.delete()
+        return HttpResponse(status=204)
