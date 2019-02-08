155a338c79aed75b8006b66b72fcd0709de8dd55
Shota Tamura <tamura@yuuniworks.com>
2019-02-08 14:34:25 +0900

    use ModelSerializer

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -2,33 +2,7 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False,
-                                  allow_blank=True,
-                                  max_length=100)
-    code = serializers.CharField(style={'base_template': 'textarea.html'})
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
-                                       default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def create(self, validated_data):
-        """
-        新しいSnippetインスタンスを作成して返す。
-        インスタンスは、バリデーション済みのデータを基にして作成する。
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        既存のSnippetインスタンスを変更して返す。
-        インスタンスは、バリデーション済みのデータを基にして変更する。
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.code = validated_data.get('code', instance.code)
-        instance.linenos = validated_data.get('linenos', instance.linenos)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
