03edb386abc7e64a34b327307a2ac1e10346e13f
Shota Tamura <tamura@yuuniworks.com>
2019-02-11 18:20:49 +0900

    hyperlink between entities

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -3,19 +3,23 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.CharField(read_only=True)
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos',
+        fields = ('url', 'id', 'highlight', 'title', 'code', 'linenos',
                   'language', 'style', 'owner')
 
 
 class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all())
+    snippets = serializers.\
+        HyperlinkedRelatedField(many=True,
+                                view_name="snippet-detail",
+                                read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'id', 'username', 'snippets')
