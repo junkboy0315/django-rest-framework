0fc20090e6be1f60f2d6ebb27fbfde26e33d8db2
Shota Tamura <tamura@yuuniworks.com>
2019-02-11 17:13:33 +0900

    add user apis(only for referencing)

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from rest_framework import generics, permissions
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -24,3 +25,13 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
